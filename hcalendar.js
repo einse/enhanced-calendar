1542ff6b3bc52f9c570389e6599c225cce59d42b
Serguey Arsentev <einse@yandex.ru>
2016-09-30 10:01:41 +0000

    Add TODOs

--- hcalendar.js
+++ hcalendar.js
@@ -1,5 +1,7 @@
 /**
- * This file was a part of Respire,
+ * HCalendar by Serguey Arsentev: http://github.com/einse/hcalendar
+ * 
+ * This file was a part of Respire (http://github.com/einse/respire),
  * but now it is not.
  */
 
@@ -70,3 +72,42 @@ var Calendar = {
 		
 	}
 };
+// TODO:
+// 1. Add C$, HC$ shortcuts?
+// 2. Global var or 'window'/'document' method?
+// TODO: Presumable examples of using:
+// C$('1900-02-29') -> should return empty string, as the date not valid
+// C$('2000-02-29') -> should return '2000-02-29'
+// C$('2000-02-29', '') -> should return '29.02.2000'
+// C$('2000-02-29', '.') -> should return the same as previous one
+// C$('2000-02-29', 'r') -> should return '2000.02.29'
+// C$('2000-02-29', 'r.') -> should return the same as previous one
+// C$('2000-02-29', '/') -> should return '29/02/2000'
+// C$('2000-02-29', 'a/') -> should return '02/29/2000'
+// C$('2000-02-29', 'r/') -> should return '2000/02/29'
+// C$('2000-02-29', 'r/2') -> should return '02/29'
+// C$('2000-02-29', 'sr/2') -> should return '2/29'
+// C$('2000-02-29', 'sr2') -> should return '2.29'
+// C$('2000-02-29', 's2') -> should return '29.2'
+// C$('2000-02-29', '2') -> should return '29.02'
+// C$('2000-02-29', 2) -> should return the same as previous one
+// C$('2000-02-29', 1) or C$('2000-02-29', '1') -> should return '29'
+// C$('2000-02-29', ' ') -> should return '29 02 2000'
+// C$('2000-02-29', 't ') -> should return '29 02 00'
+// C$('2000-02-29', 't') -> should return '29.02.00'
+// C$('2000-02-29', 'r.2') -> should return '02.29'
+// C$('2000-02-29', 'r-2') -> should return '02-29'
+// C$('2000-02-29', '/2') -> should return '29/02'
+// C$('2000-02-29', '.2') -> should return '29/02'
+// C$('2000-02-29', '-2') -> should return '29-02'
+// C$('2000-02-29', '2') -> should return '29/02'
+// C$('2000-02') -> ...
+// C$('2000-02', 'ru') -> ...
+// C$('2000') -> ...
+// C$.weekday('2000-03-01') -> 'Wed' or '3'?
+// TODO: Also to learn: 
+// https://en.wikipedia.org/wiki/Date_format_by_country
+// https://en.wikipedia.org/wiki/ISO_8601
+// https://en.wikipedia.org/wiki/Date_and_time_representation_by_country
+// 
+// Posted 30.09.2016
